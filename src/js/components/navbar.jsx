95d95da772fad56f1ff252767305eb74b9af32df
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-07-21 22:22:13 +0000

    cierre

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg d-flex mb-3" style={{ backgroundColor: 'black' }}>
             <div className="container-fluid" style={{ maxWidth: '1320px' }}>
-                <a className="navbar-brand" href="#" style={{ color: 'white' }}>Start Bootstrap</a>
+                <a className="navbar-" href="#" style={{ color: 'white' }}>Start Bootstrap</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" >
                     <span className="navbar-toggler-icon"></span>
                 </button>
